0f9bc6d6b65662dcdbd9c148b990c3da81326957
Iwan van der Kleijn <iwanvanderkleijn@gmail.com>
2024-01-28 20:04:28 +0100

    Refactoring to module with haiku example

---
+++ aitheneum/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+AITheneum - A playground for study of and practice with all matters related with LLM's and Langchain"
+@copyright: Copyright © 2024 Iwan van der Kleijn
+@license: MIT
+"""
+
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+#set the key from file "openai_key.txt" in the same directory as this file or set the environment variable OPENAI_API_KEY
+load_dotenv("openai_api_key.env")
